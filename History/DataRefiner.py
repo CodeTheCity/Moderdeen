2d3aafabc5ea03cb3c0aba8517deec3f5e00c4a1
Arman511 <114365556+Arman511@users.noreply.github.com>
2023-09-23 16:54:30 +0100

    fixed null names

--- History/DataRefiner.py
+++ History/DataRefiner.py
@@ -6,6 +6,7 @@ def extract_data(json_data):
         id_value = element.get("id", None)
         name = element["tags"].get("name", None)
         alt_name = element["tags"].get("alt_name", None)
+        old_name = element["tags"].get("old_name", None)
 
         # Use the first non-empty name (prefer "name" over "alt_name")
         if name:
@@ -25,11 +26,17 @@ def extract_data(json_data):
         building_number = element["tags"].get("addr:housenumber", None)
         postcode = element["tags"].get("addr:postcode", None)
 
+        # Check if there's no name but an old_name
+        if not name and old_name:
+            name = old_name  # Use old_name as the name
+            original_name = "Closed: "+old_name.replace('\u2019', "'")  # Keep the original casing
+
         # Check if this item with the same name already exists (case-insensitive comparison)
         if name in extracted_data:
-            # Check if the current item has an older timestamp
-            if timestamp < extracted_data[name]["timestamp"]:
-                # Update the existing item with the older information
+            existing_item = extracted_data[name]
+            # Check if the current item has a newer timestamp
+            if timestamp > existing_item["timestamp"]:
+                # Update the existing item with the newer information
                 extracted_data[name] = {
                     "id": id_value,
                     "name": original_name,  # Keep the original casing
@@ -38,8 +45,13 @@ def extract_data(json_data):
                     "amenity": amenity,
                     "lat": lat,
                     "lon": lon,
-                    "building_number": building_number,
-                    "postcode": postcode,
+                    "building_number": (
+                        building_number if building_number is not None else existing_item["building_number"]
+                    ),
+                    "postcode": (
+                        postcode if postcode is not None else existing_item["postcode"]
+                    ),
+                    "note": element.get("tags").get("note", None),  # Add the "note" field
                 }
         else:
             # This is the first item with this name, add it to the dictionary
@@ -53,6 +65,7 @@ def extract_data(json_data):
                 "lon": lon,
                 "building_number": building_number,
                 "postcode": postcode,
+                "note": element.get("tags").get("note", None),  # Add the "note" field
             }
 
     # Convert the dictionary values to a list of items, excluding the lowercase name
@@ -68,6 +81,7 @@ def extract_data(json_data):
             "lon": value["lon"],
             "building_number": value["building_number"],
             "postcode": value["postcode"],
+            "note": value["note"],  # Include the "note" field
         })
 
     return extracted_data_list
