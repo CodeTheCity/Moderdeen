58dd7df505a9df478adfe4a2e367c033a8085108
Arman511 <114365556+Arman511@users.noreply.github.com>
2023-09-23 18:21:04 +0100

    redone the refiner to count more outliers

--- History/DataRefiner.py
+++ History/DataRefiner.py
@@ -7,6 +7,9 @@ def extract_data(json_data):
         name = element["tags"].get("name", None)
         alt_name = element["tags"].get("alt_name", None)
         old_name = element["tags"].get("old_name", None)
+        amenity = element["tags"].get("amenity", None)
+        shop = element["tags"].get("shop", None)
+        disused_shop = element["tags"].get("disused:shop", None)
 
         # Use the first non-empty name (prefer "name" over "alt_name")
         if name:
@@ -15,21 +18,29 @@ def extract_data(json_data):
         elif alt_name:
             original_name = alt_name.replace('\u2019', "'")
             name = alt_name.lower()
+        elif old_name:
+            name = old_name  # Use old_name as the name
+            original_name = "Closed: " + old_name.replace('\u2019', "'")  # Keep the original casing
         else:
-            name = None
+            name = "unknown"
             original_name = None
 
         timestamp = element.get("timestamp", None)
-        amenity = element["tags"].get("amenity", None)
         lat = element.get("lat", None)
         lon = element.get("lon", None)
         building_number = element["tags"].get("addr:housenumber", None)
         postcode = element["tags"].get("addr:postcode", None)
 
-        # Check if there's no name but an old_name
-        if not name and old_name:
-            name = old_name  # Use old_name as the name
-            original_name = "Closed: "+old_name.replace('\u2019', "'")  # Keep the original casing
+        # Check if amenity is null but "shop" is present, use "shop" as amenity
+        if amenity is None and shop:
+            amenity = shop
+
+        # Check if disused:shop is "yes" and add "Disused" prefix to the name
+        if disused_shop == "yes":
+            if name != "unknown":
+                name = "Disused " + name
+            if original_name:
+                original_name = "Disused " + original_name
 
         # Check if this item with the same name already exists (case-insensitive comparison)
         if name in extracted_data:
@@ -51,7 +62,7 @@ def extract_data(json_data):
                     "postcode": (
                         postcode if postcode is not None else existing_item["postcode"]
                     ),
-                    "note": element.get("tags").get("note", None),  # Add the "note" field
+                    "note": element.get("tags").get("note", "N/A"),  # Add the "note" field
                 }
         else:
             # This is the first item with this name, add it to the dictionary
@@ -65,7 +76,7 @@ def extract_data(json_data):
                 "lon": lon,
                 "building_number": building_number,
                 "postcode": postcode,
-                "note": element.get("tags").get("note", None),  # Add the "note" field
+                "note": element.get("tags").get("note", "N/A"),  # Include the "note" field
             }
 
     # Convert the dictionary values to a list of items, excluding the lowercase name
